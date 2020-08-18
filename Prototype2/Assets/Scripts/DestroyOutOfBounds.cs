5165a783db361ac35636a034890b5f497e9241e6
Ayren Jagar <ayrenjagar@gmail.com>
2020-08-19 00:08:26 +0200

    Completed Lesson 2.2

---
+++ Prototype2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+
+    private float topBound = 30;
+    private float lowerBound = -15;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Destroy gameObject if out of bounds
+        if(transform.position.z  > topBound)
+        {
+            Destroy(gameObject);
+        }
+        else if(transform.position.z < lowerBound)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Prototype2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private float horizontalInput;
+    private float speed = 30.0f;
+    private float xRange = 12;
+
+    public GameObject projectilePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Keep player in bounds
+        if(transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        else if(transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            // Launch a projectile from player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+
+        // Get player input
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        // Move player horizontal
+        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+    }
+}
