f4a97f08047fe65ea3bb704229b9c07f38371d93
Ayren Jagar <ayrenjagar@gmail.com>
2020-08-18 22:35:23 +0200

    Week1 Prototype1 initial commit
    Initial Commit

---
+++ Prototype1/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+    public GameObject player;
+    private Vector3 offSet = new Vector3(0, 5, -10);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Follow player object with offset
+        transform.position = player.transform.position + offSet;
+    }
+}
